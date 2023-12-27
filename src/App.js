3af881af74631fd9061cdfe31e7f16849ac38ce3
tarang1998 <tarangnair98@gmail.com>
2023-12-27 18:42:54 -0500

    Creating the project dashboard page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path='/'element={<Dashboard/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
