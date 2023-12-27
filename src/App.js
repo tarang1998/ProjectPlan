18febad373d81a3b4e5816eba287194ba1ec0da9
tarang1998 <tarangnair98@gmail.com>
2023-12-26 20:48:11 -0500

    Setting up the initial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Project Plan</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
