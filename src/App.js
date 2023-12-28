ddb9fdbbdb3e8f4c86232f0339e7263956d03650
tarang1998 <tarangnair98@gmail.com>
2023-12-27 19:35:31 -0500

    Added project summary and project details component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Routes>
-            <Route path='/'element={<Dashboard/>} />
+            <Route exact path='/'element={<Dashboard/>} />
+            <Route path='/project/:id' element={<ProjectDetails/>}/>
           </Routes>
         </div>
       </BrowserRouter>
