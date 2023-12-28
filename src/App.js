d330d774b8563647a3bcb48be66d852eccc472c1
tarang1998 <tarangnair98@gmail.com>
2023-12-28 00:56:52 -0500

    Created the signup and the signin components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -13,6 +15,9 @@ class App extends Component {
           <Routes>
             <Route exact path='/'element={<Dashboard/>} />
             <Route path='/project/:id' element={<ProjectDetails/>}/>
+            <Route path='/signin' element={<SignIn/>}/>
+            <Route path='/signup  ' element={<SignUp/>}/>
+
           </Routes>
         </div>
       </BrowserRouter>
